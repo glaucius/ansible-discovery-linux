4f003bd95c7b516e4b27e41b7e0e0f3b2c4e5087
glaucius <glaucius@gmail.com>
2019-04-08 01:35:51 -0300

    mais correções genéricas e melhorias

--- discovery.sh
+++ discovery.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-rm -rf /tmp/discovery
-mkdir -p /tmp/discovery
 _hostname=`hostname`
-echo HOSTNAME > /tmp/stage-discovery/$_hostname.discovery.txt
-hostname > /tmp/stage-discovery/$_hostname.discovery.txt
-echo UPTIME > /tmp/stage-discovery/$_hostname.discovery.txt
-uptime >> /tmp/stage-discovery/$_hostname.discovery.txt
-echo MEM > /tmp/stage-discovery/$_hostname.discovery.txt
-free >> /tmp/stage-discovery/$_hostname.discovery.txt
-echo DISCOS > /tmp/stage-discovery/$_hostname.discovery.txt
-df -h >> /tmp/stage-discovery/$_hostname.discovery.txt
-
-#ok
+echo HOSTNAME > /tmp/stage-discovery/logs/$_hostname.discovery.txt
+hostname > /tmp/stage-discovery/logs/$_hostname.discovery.txt
+echo UPTIME > /tmp/stage-discovery/logs/$_hostname.discovery.txt
+uptime >> /tmp/stage-discovery/logs/$_hostname.discovery.txt
+echo MEM > /tmp/stage-discovery/logs/$_hostname.discovery.txt
+free >> /tmp/stage-discovery/logs/$_hostname.discovery.txt
+echo DISCOS > /tmp/stage-discovery/logs/$_hostname.discovery.txt
+df -h >> /tmp/stage-discovery/logs/$_hostname.discovery.txt
