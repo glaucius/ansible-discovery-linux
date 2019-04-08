65a07a7b28dcd2f9ce752b0a14634f946a87c8a1
glaucius <glaucius@gmail.com>
2019-04-08 00:33:34 -0300

    Fix e correções

--- discovery.sh
+++ discovery.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+rm -rf /tmp/discovery
 _hostname=`hostname`
-echo HOSTNAME > /tmp/$_hostname.discovery.txt
-hostname > /tmp/$_hostname.discovery.txt
-echo UPTIME > /tmp/$_hostname.discovery.txt
-uptime >> /tmp/$_hostname.discovery.txt
-echo MEM > /tmp/$_hostname.discovery.txt
-free >> /tmp/$_hostname.discovery.txt
-echo DISCOS > /tmp/$_hostname.discovery.txt
-df -h >> /tmp/$_hostname.discovery.txt
+echo HOSTNAME > /tmp/discovery/$_hostname.discovery.txt
+hostname > /tmp/discovery/$_hostname.discovery.txt
+echo UPTIME > /tmp/discovery/$_hostname.discovery.txt
+uptime >> /tmp/discovery/$_hostname.discovery.txt
+echo MEM > /tmp/discovery/$_hostname.discovery.txt
+free >> /tmp/discovery/$_hostname.discovery.txt
+echo DISCOS > /tmp/discovery/$_hostname.discovery.txt
+df -h >> /tmp/discovery/$_hostname.discovery.txt
 
