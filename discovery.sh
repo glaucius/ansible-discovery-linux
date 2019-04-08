c180ec76429209587ca5372b257e366a17ca071c
glaucius <glaucius@gmail.com>
2019-04-08 09:18:07 -0300

    melhorias, bugs e afins

--- discovery.sh
+++ discovery.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 _hostname=`hostname`
-echo HOSTNAME > /tmp/stage-discovery/logs/$_hostname.discovery.txt
-hostname > /tmp/stage-discovery/logs/$_hostname.discovery.txt
-echo UPTIME > /tmp/stage-discovery/logs/$_hostname.discovery.txt
-uptime >> /tmp/stage-discovery/logs/$_hostname.discovery.txt
-echo MEM > /tmp/stage-discovery/logs/$_hostname.discovery.txt
-free >> /tmp/stage-discovery/logs/$_hostname.discovery.txt
-echo DISCOS > /tmp/stage-discovery/logs/$_hostname.discovery.txt
-df -h >> /tmp/stage-discovery/logs/$_hostname.discovery.txt
+echo "###### HOSTNAME : $_hostname"
+hostname 
+echo "###### UPTIME"
+uptime 
+echo "###### MEM "
+free 
+echo "###### DISCOS "
+df -h 
