c79727d6ff1b1d5bb9ca0149b9d284f0756e3c44
glaucius <glaucius@gmail.com>
2019-04-08 01:24:24 -0300

    melhorias

--- discovery.sh
+++ discovery.sh
@@ -2,13 +2,13 @@
 rm -rf /tmp/discovery
 mkdir -p /tmp/discovery
 _hostname=`hostname`
-echo HOSTNAME > /tmp/discovery/$_hostname.discovery.txt
-hostname > /tmp/discovery/$_hostname.discovery.txt
-echo UPTIME > /tmp/discovery/$_hostname.discovery.txt
-uptime >> /tmp/discovery/$_hostname.discovery.txt
-echo MEM > /tmp/discovery/$_hostname.discovery.txt
-free >> /tmp/discovery/$_hostname.discovery.txt
-echo DISCOS > /tmp/discovery/$_hostname.discovery.txt
-df -h >> /tmp/discovery/$_hostname.discovery.txt
+echo HOSTNAME > /tmp/stage-discovery/$_hostname.discovery.txt
+hostname > /tmp/stage-discovery/$_hostname.discovery.txt
+echo UPTIME > /tmp/stage-discovery/$_hostname.discovery.txt
+uptime >> /tmp/stage-discovery/$_hostname.discovery.txt
+echo MEM > /tmp/stage-discovery/$_hostname.discovery.txt
+free >> /tmp/stage-discovery/$_hostname.discovery.txt
+echo DISCOS > /tmp/stage-discovery/$_hostname.discovery.txt
+df -h >> /tmp/stage-discovery/$_hostname.discovery.txt
 
 #ok
